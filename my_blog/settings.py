f2204045850e9ec3ce09f0b28207be9e378a54db
jhao104 <946150454@qq.com>
2016-10-12 21:03:41 -0400

    do not commit setting.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'my_blog',
         'USER': 'root',
-        'PASSWORD': '5849312',
+        'PASSWORD': 'jinghao',
         'HOST': 'localhost',
         'PORT': '3306'
     }
