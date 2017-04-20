3ee4dda9c021390858b8d1a73791f9071aa2ba61
jhao104 <946150454@qq.com>
2017-04-20 17:21:27 +0800

    [update]修改首页

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '85bus3!*+_3t1ks)&&o(u-liuhucshja!-44a1squ93=#+v+4r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog'))
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'', views.Index)
 ]
