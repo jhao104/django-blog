49bac0701c958ae614ea5969e4de1b74a07e47d0
jinghao_wb <jinghao_wb@g7.com.cn>
2017-08-28 15:43:09 +0800

    [add] add robots

--- blog/urls.py
+++ blog/urls.py
@@ -22,10 +22,10 @@ urlpatterns = [
     url(r'^archive/$', views.archive, name='archive'),
     url(r'^link/$', views.Link, name='link'),
     url(r'^message$', views.Message, name='message'),
-    url(r'^article/(?P<pk>\d+)$', views.Articles, name='article'),
+    url(r'^article/(?P<pk>\d+)/$', views.Articles, name='article'),
     url(r'^get_comment/$', views.GetComment, name='get_comment'),
-    url(r'^detail/(?P<pk>\d+)$', views.detail, name='detail'),
+    url(r'^detail/(?P<pk>\d+)/$', views.detail, name='detail'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^tag/(?P<name>.*?)$', views.tag, name='tag'),
+    url(r'^tag/(?P<name>.*?)/$', views.tag, name='tag'),
     url(r'.*?$', views.Index, name='index'),
 ]
