74bdc59c1d0030303747ebcffdea5f7e9391cef5
jinghao_wb <jinghao_wb@g7.com.cn>
2017-08-28 17:20:27 +0800

    [update]修改路由

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -20,5 +20,5 @@ from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls', namespace='blog')),
-    url(r'', views.Index)
+    url(r'^$', views.Index)
 ]
